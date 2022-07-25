cc87a89d71d7fd020bce2e2120c1b82b2b2131f4
alybali <alybalistreri@gmail.com>
2022-07-25 17:22:46 -0500

    first commit

---
+++ sexy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+RED="\e[31m"
+ENDCOLOR="\e[0m"
+ITALICRED="\e[3;${RED}m"
+GREEN="\e[32m"
+echo -e "${RED} You're a fucking savage, ${ENDCOLOR} "
+echo -e "${GREEN} doggo ${ENDCOLOR}"
+
+
